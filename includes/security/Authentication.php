ebc8f833a661201a27d58a6b9a3577a787213f4b
MatinAbTerm3 <79203163+MatinAbTerm3@users.noreply.github.com>
2021-06-23 20:41:03 +0430

    Add files via upload

---
+++ includes/security/Authentication.php
@@ -0,0 +1,44 @@
+<?php
+if(! class_exists('Authentication')) {
+	class Authentication{
+		static public function login( $uid ){
+			
+			$_SESSION['uid'] = $uid;
+			
+			$_SESSION['lastLogin'] = time();
+		}
+		
+		static public function check(){
+			return isset( $_SESSION['uid'] );
+		}
+		
+		static public function logout(){
+	
+			unset( $_SESSION['uid'] );
+		}
+		
+		static public function uid(){
+			if( self :: check() )
+				return $_SESSION['uid'];
+			else
+				return false;
+		}
+		
+		static public function autoLogout(){
+			if( self :: check() ){
+				if( time() - $_SESSION['lastLogin'] > LOGIN_DEADLINE * 24 * 60 * 60 ){ // 30 days
+					Alert::alerts('با ورود مجدد به ما کمک کتید امنیت شما را تامین کنیم!', 'warning');
+					$_SESSION['redirect'] = $_SERVER['REQUEST_URI'];
+					self :: logout();
+				}
+					
+				if( time() - $_SESSION['lastActivity'] > ACTIVITY_DEADLINE * 60){ // 10 min
+					echo alertTemplate('مدتی غیرفعال بودید، لطفا مجددا وارد شوید!', 'warning');
+					$_SESSION['redirect'] = $_SERVER['REQUEST_URI'];
+					self :: logout();
+				}	
+				$_SESSION['lastActivity'] = time();
+			}
+		}
+	}
+}
